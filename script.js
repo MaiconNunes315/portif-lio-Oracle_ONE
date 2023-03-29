aa5e1320d9d3931127b310a5cd156d6b9c1c6c42
Maicon Nunes <mnunes315@hotmail.com>
2023-03-29 17:39:11 -0300

    Validação do formulário

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { infos } from "./infos.js";
 import { className } from "./className.js"
 
 
-(async function () {
+(async () => {
 
     const getUrl = fetch('https://api.github.com/users/maiconnunes315/repos?per_page=2');
 
@@ -16,5 +16,62 @@ import { className } from "./className.js"
     })
 })()
 
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const menssage = document.getElementById("menssage");
 
 
+function sendEmail() {
+
+    Email.send({
+        SecureToken: "b944f4fd-9d13-48f8-a489-4d2fea9a370e",
+        To: "mnunes315@hotmail.com",
+        From: "mnunes315@hotmail.com",
+        Subject: subject.value,
+        Body: `Olá ! Meu nome é ${name.value} meu email ${email.value} 
+        \n \n ${menssage.value}`
+    }).then(() => alert("Email enviado com sucesso ! "))
+
+}
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input, index) => {
+
+    input.addEventListener('blur', (event) => {
+        validationForm(event.target, index)
+    })
+
+})
+
+function validationForm(input, index) {
+    const errorMenssage = document.querySelectorAll('.menssage_error')[index]
+    console.log(input.validity)
+    if (!input.validity.valid) {
+        errorMenssage.classList.add('visible')
+    } else {
+        errorMenssage.classList.remove('visible')
+    }
+}
+
+document.querySelector(".contact_button").addEventListener("click", function (event) {
+
+    let checkValidation = 0;
+
+    inputs.forEach((input, index) => {
+
+        const errorMenssage = document.querySelectorAll('.menssage_error')[index]
+        if (input.validity.valid && !input.validity.typeMismatch) {
+            checkValidation++;
+        } else {
+            errorMenssage.innerHTML = "Informação incorreta"
+        }
+    })
+
+    if (checkValidation == 3) {
+        sendEmail()
+    }
+
+
+});
